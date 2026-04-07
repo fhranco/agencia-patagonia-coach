efb78224f9a66d6f44dd16dfc0ea7f0c3b0f9923
fhranco <patagoniacoach@gmail.com>
2026-04-07 01:21:59 -0300

    UI Redesign: Horizontally centered the FloatingActions and LeadAgent hub for a unified 'App-like' command center experience.

--- src/components/LeadAgent.jsx
+++ src/components/LeadAgent.jsx
@@ -48,7 +48,7 @@ const LeadAgent = ({ isOpen, onClose }) => {
   return (
     <AnimatePresence>
       {isOpen && (
-        <div className="fixed bottom-28 right-28 z-[510]">
+        <div className="fixed bottom-28 left-1/2 -translate-x-1/2 z-[510]">
           <motion.div
             initial={{ opacity: 0, scale: 0.95, y: 20 }}
             animate={{ opacity: 1, scale: 1, y: 0 }}
